da1920dcae80406f5457d5a236124ccc571a1878
Ali Tofigh <alix.tofigh@gmail.com>
2008-03-04 13:24:46 +0000

    small change in usage string
    git-svn-id: file:///bioinfo/home/alix/repositories/phyltr.svn/trunk@8 5bcd4aa6-42ea-41a5-9ec1-3b4da75a86a9

--- src/phyltr-fpt.cc
+++ src/phyltr-fpt.cc
@@ -107,7 +107,7 @@ struct Program_input {
 
 const string PROG_NAME = "phyltr-fpt";
 const string USAGE = "Usage: " + PROG_NAME + 
-    " [OPTION]... SPECIES_TREE_FILE GENE_TREE_FILE GENE_MAP_FILE\n"
+    " [OPTION]... SPECIES_TREE_FILE GENE_TREE_FILE MAP_FILE\n"
     "                  MIN_COST MAX_COST";
 
 Program_input g_program_input = Program_input();
